8d8538c3d326c8b68d46477c0ce64830f78bace7
kim-ly <SoSe2018>
2019-09-05 15:49:09 +0200

    Initial commit, implements UI

---
+++ app/src/main/java/com/example/logicgame/LogicGameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logicgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogicGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.logic_game_main)
+    }
+}
